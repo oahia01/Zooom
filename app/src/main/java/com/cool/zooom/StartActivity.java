805af362784a505e69796fbf00ac85d7ded7f257
oahia <oghenefego@2mar.com>
2015-09-19 14:25:53 -0400

    First set of activities and some layout design

--- app/src/main/java/com/cool/zooom/StartActivity.java
+++ app/src/main/java/com/cool/zooom/StartActivity.java
@@ -1,16 +1,37 @@
 package com.cool.zooom;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class StartActivity extends AppCompatActivity {
+
+    private static final String TAG = StartActivity.class.getSimpleName();
+    private String distanceMatrixRequestAddress =
+            "https://maps.googleapis.com/maps/api/distancematrix/json?parameters";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        //remove actionbar
     }
 
     @Override
@@ -34,4 +55,52 @@ public class StartActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void HttpPOST(final String data) {
+        try {
+            RequestQueue requestQueue = ZooomApp.getRequestQueue();
+
+            Map<String, String> params = null;
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                    "address",
+                    new JSONObject(params),
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                final String HttpResponseStr = response.getString("status");
+//                                handleNetworkResponse(HttpResponseStr);
+                            } catch (JSONException e) {
+                                Log.e(TAG, "Could not convert JSONObject Response to a String and handle the data");
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e(TAG, "Could not post to CHECK_USER_REGISTRATION_SERVER. Error: " + error);
+                }
+            }) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    HashMap<String, String> headers = new HashMap<String, String>();
+                    headers.put("Content-Type", "application/json");
+                    headers.put("charset", "utf-8");
+                    return headers;
+                }
+            };
+
+            int socketTimeout = 30000;
+            RetryPolicy policy = new DefaultRetryPolicy(socketTimeout,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+            jsonObjectRequest.setRetryPolicy(policy);
+            jsonObjectRequest.setTag(TAG);
+//            ZooomSingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+
+        } catch (Exception e) {
+            Log.e(TAG, "Could not perform successful HttpPost");
+        }
+    }
+
 }
---
+++ app/src/main/java/com/cool/zooom/ZooomSingleton.java
@@ -0,0 +1,16 @@
+package com.cool.zooom;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by Oghenefego on 9/19/2015.
+ */
+public class ZooomSingleton {
+
+
+//    public static Application getInstance(Context context){
+//
+//    }
+
+}
